188287e39ff770bc09ad7b3eb7f7320157e5bf1b
narenjava942 <narenjava942@gmail.com>
2025-08-11 19:32:34 +0530

    latest changes in readme file and removed rewardtest directory

--- rewardsapi/src/main/java/com/example/rewardsapi/controller/RewardController.java
+++ rewardsapi/src/main/java/com/example/rewardsapi/controller/RewardController.java
@@ -18,7 +18,6 @@ public class RewardController {
 
     public RewardController(RewardService rewardService) { this.rewardService = rewardService; }
 
-    // GET /api/rewards?start=2025-05&end=2025-07 -> returns rewards for all customers between start and end months
     @GetMapping
     public ResponseEntity<List<RewardResponseDto>> getAll(@RequestParam String start, @RequestParam String end) {
         YearMonth startMonth = YearMonth.parse(start);
@@ -26,7 +25,6 @@ public class RewardController {
         return ResponseEntity.ok(rewardService.getAllRewards(startMonth, endMonth));
     }
 
-    // GET /api/rewards/{customerId}?start=2025-05&end=2025-07
     @GetMapping("/{customerId}")
     public ResponseEntity<RewardResponseDto> getForCustomer(@PathVariable String customerId, @RequestParam String start, @RequestParam String end) {
         YearMonth startMonth = YearMonth.parse(start);
